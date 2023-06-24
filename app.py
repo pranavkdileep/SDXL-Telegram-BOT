cb93b8d77c80e351b0917148742c8370751e8ad0
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2023-06-24 03:07:22 +0000

    gfgh

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import telebot
 
+bottoken = "1836057993:AAF4cP9qNx3gFh-epkgjBRYkALyzVs6W0gw"
+
+bot = telebot.TeleBot(bottoken)
+
+@bot.message_handler(commands=["start","hello"])
+def send_wlcomemesssage(message):
+    bot.reply_to(message,"Hello")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
