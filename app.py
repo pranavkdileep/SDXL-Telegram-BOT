a68e2f158677c8a01ad8c04ec83351b0ac5f3570
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2023-06-24 06:04:37 +0000

    hello

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import telebot
+import os
+from get import get_truecaller_info
 
-bottoken = "1836057993:AAF4cP9qNx3gFh-epkgjBRYkALyzVs6W0gw"
+bottoken = os.environ['bottoken']
 
 bot = telebot.TeleBot(bottoken)
 
-@bot.message_handler(commands=["start","hello"])
+@bot.message_handler(commands=["start","hello","help"])
 def send_wlcomemesssage(message):
-    bot.reply_to(message,"Hello")
+    bot.reply_to(message,"Hey I Will Help You to find Phone Numbers Details just send phone number in international format eg:-  +919999999999")
 
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    # i need to remove spacese from message and convert it to string
+    phone_numbert = message.text.replace(" ", "")
+    # check if the message is a number with + sign
+    if phone_numbert.startswith("+") and phone_numbert[1:].isdigit():
+        dtails = get_truecaller_info(phone_numbert)
+        bot.reply_to(message, dtails)
+    else:
+        bot.reply_to(message, "Please send a valid phone number")
 
 
 bot.infinity_polling()
--- get.py
+++ get.py
@@ -1,32 +1,21 @@
 from truecallerpy import search_phonenumber
 
-def get_truecaller_info(phone_number, country_code, api_key):
+def get_truecaller_info(phone_number):
+    api_key = "a1i0u--gFEQ8K-D-vzYJeE8wvTQhcKEFbXpERdfDZChE1Y4mcY1LvG7hK6r9V8HV"
+    country_code = "IN"
     data = search_phonenumber(phone_number, country_code, api_key)
+    
 
     # Extracting and returning the desired information
     if 'data' in data and len(data['data']) > 0:
         result = data['data'][0]
-        name = result['name']
-        email = result['internetAddresses'][0]['id']
-        country = result['addresses'][0]['countryCode']
-        city = result['addresses'][0]['city']
-
-        return name, email, country, city
+        name = result.get('name')
+        email = result['internetAddresses'][0]['id'] if result['internetAddresses'] else None
+        country = result['addresses'][0].get('countryCode')
+        city = result['addresses'][0].get('city')
+        info = f"Name: {name}\nEmail: {email}\nCountry: {country}\nCity: {city}"
+        return info
     else:
         return None
 
 # Example usage
-api_key = "a1i0u--gFEQ8K-D-vzYJeE8wvTQhcKEFbXpERdfDZChE1Y4mcY1LvG7hK6r9V8HV"
-phone_number = "+919544047655"
-country_code = "IN"
-
-info = get_truecaller_info(phone_number, country_code, api_key)
-
-if info:
-    name, email, country, city = info
-    print("Name:", name)
-    print("Email:", email)
-    print("Country:", country)
-    print("City:", city)
-else:
-    print("No information available for the provided phone number.")
