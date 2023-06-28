781b6f6b5b9e1a71328c0e6822641b0cb5f0cd5f
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-06-28 11:08:19 +0000

    ew

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import subprocess
+from get import get_truecaller_info
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def run_app():
-    subprocess.Popen(['python', 'app.py'])
-    return 'App running forever'
+    phone_number = request.args.get('phone_number')
+    if not phone_number:
+        subprocess.Popen(['python', 'app.py'])
+        return jsonify({'error': 'phone_number is required'})
+    
+    info = get_truecaller_info(phone_number)
+    return info
 
 if __name__ == '__main__':
     app.run()
